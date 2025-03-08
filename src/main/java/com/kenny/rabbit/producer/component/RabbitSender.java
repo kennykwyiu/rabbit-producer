237182e793f0c295dc69d84494657cafadb317f5
kenny <kenny.kwyiu@gmail.com>
2025-03-08 18:09:04 +0800

    send

--- src/main/java/com/kenny/rabbit/producer/component/RabbitSender.java
+++ src/main/java/com/kenny/rabbit/producer/component/RabbitSender.java
@@ -1,12 +1,19 @@
 package com.kenny.rabbit.producer.component;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-
+import java.util.UUID;
+@Slf4j
 @Component
 public class RabbitSender {
 
@@ -29,5 +36,36 @@ public class RabbitSender {
         }
     };
 
+    /**
+     * Sends a message to the RabbitMQ exchange with custom properties and confirmation handling.
+     *
+     * @param message    The actual message payload to send.
+     * @param properties Additional properties (headers) to be included in the message.
+     * @throws Exception If any error occurs during message processing.
+     */
+    public void send (Object message, Map<String, Object> properties) throws Exception {
+
+        MessageHeaders mhs = new MessageHeaders(properties);
+        Message<?> msg = MessageBuilder.createMessage(message, mhs);
+
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+
+        MessagePostProcessor mpp = new MessagePostProcessor() {
+            @Override
+            public org.springframework.amqp.core.Message postProcessMessage(org.springframework.amqp.core.Message message) throws AmqpException {
+                log.info("---> post to do: "  + message);
+                return message;
+            }
+        };
+
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.convertAndSend("exchange-1",
+                "springboot.rabbit",
+                msg,
+                mpp,
+                correlationData
+        );
+
+    }
 
 }
